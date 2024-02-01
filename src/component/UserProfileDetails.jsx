cdbb4c3fe1a0806d6f27802816526607b7ba8f97
vendhan <vendhan7799@gmail.com>
2024-02-01 17:31:46 +0530

    implemented projects card layout

--- src/component/UserProfileDetails.jsx
+++ src/component/UserProfileDetails.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const UserProfileDetails = () => {
   const user = useSelector((state) => state.user.user);
-  const [isMenuVisible, setIsMenuVisible] = useState(false)
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   return (
     <div className="d-flex justify-content-center align-items-center  gap-2 position-relative ">
       <motion.div
@@ -23,37 +23,45 @@ const UserProfileDetails = () => {
         ) : (
           <>
             <p className="text-capitalize text-white d-flex justify-content-center align-items-center pt-3  w-100 h-100">
-              <span style={{fontSize:"1.4rem"}}>{user?.email[0]}</span>
+              <span style={{ fontSize: "1.4rem" }}>{user?.email[0]}</span>
             </p>
           </>
         )}
       </motion.div>
       <AnimatePresence>
-      {isMenuVisible && 
-
-      <motion.div
-        initial={{opacity:0 , y:50}}
-        animate={{opacity:1 , y:0}}
-        exit={{opacity:0 , y:50}}
-        className="bg-dark px-4 py-3 rounded d-flex flex-column shadow-sm  justify-content-between align-items-center bg-dark m-2 rounded-right d-flex z-3"
-        style={{ minWidth: "152px",position:'absolute',
-        right:'-20px',
-        top:'3rem'}}
-      >
-        {Menus &&
-          Menus.map((menu) => (
-            <Link to={menu.uri} key={menu.id} className="text-decoration-none text-white-50 py-2 "
+        {isMenuVisible && (
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            className="bg-dark px-4 py-3 rounded d-flex flex-column shadow-sm  justify-content-between align-items-center bg-dark m-2 rounded-right d-flex z-3"
+            style={{
+              minWidth: "152px",
+              position: "absolute",
+              right: "-20px",
+              top: "3rem",
+            }}
+          >
+            {Menus &&
+              Menus.map((menu) => (
+                <Link
+                  to={menu.uri}
+                  key={menu.id}
+                  className="text-decoration-none text-white-50 py-2 "
+                >
+                  {menu.name}
+                </Link>
+              ))}
+            <motion.p
+              onClick={signOutAction}
+              whileTap={{ scale: 0.9 }}
+              className="text-white-50 pt-2"
+              style={{ cursor: "pointer" }}
             >
-              {menu.name}
-            </Link>
-          ))}
-          <motion.p
-          onClick={signOutAction}
-          whileTap={{scale:0.9}} 
-          className="text-white-50 pt-2"
-          style={{cursor:'pointer'}}
-          >sign out</motion.p>
-      </motion.div>}
+              sign out
+            </motion.p>
+          </motion.div>
+        )}
       </AnimatePresence>
     </div>
   );
--- src/container/Projects.jsx
+++ src/container/Projects.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+import { MdBookmark } from "react-icons/md";
 
 const Projects = () => {
+  const projects = useSelector((state) => state.projects?.projects);
+  console.log(projects);
   return (
-    <div>Projects</div>
-  )
-}
-
-export default Projects
+    <div className="w-100 d-flex gap-4 flex-wrap justify-content-center align-items-start py-3 overflow-auto">
+      {projects &&
+        projects.map((project, index) => (
+          <ProjectCard key={project.id} project={project} index={index} />
+        ))}
+    </div>
+  );
+};
+const ProjectCard = ({ project, index }) => {
+  return (
+    <motion.div
+      key={index}
+      className="bg-dark p-2 d-flex justify-content-center align-items-center flex-column rounded overflow-hidden"
+      style={{ width: "400px", height: "325px", cursor: "pointer" }}
+    >
+      <div
+        className="h-100 w-100 bg-secondary overflow-hidden rounded "
+        style={{ overflow: "hidden" }}
+      >
+        <iframe
+          srcDoc={project.output}
+          title="output"
+          style={{ border: "none", width: "100%", height: "100%" }}
+        />
+      </div>
+      <div className="d-flex justify-content-start align-items-center w-100 gap-2 p-2">
+        <div
+          className="d-flex justify-content-center align-items-center rounded-circle overflow-hidden bg-secondary pt-3 px-3"
+          style={{ width: "3rem", height: "3rem" }}
+        >
+          {project?.user?.photoURL ? (
+            <>
+              <img
+                src={project?.user?.photoURL}
+                className="object-fit-cover w-100 h-100"
+              />
+            </>
+          ) : (
+            <>
+              <p className="text-capitalize text-white d-flex justify-content-center align-items-center w-100 h-100">
+                <span style={{ fontSize: "1.4rem" }}>
+                  {project?.user?.email[0]}
+                </span>
+              </p>
+            </>
+          )}
+        </div>
+        <div className="d-flex justify-content-between  w-100 align-items-center">
+          <div>
+          <p className="text-white p-0 m-0"alert>{project?.title}</p>
+          <p className="text-white-50 p-0 m-0">
+            {project?.user?.displayName
+              ? project?.user?.displayName
+              : `${project?.user?.email.split("@")[0]}`}
+          </p>
+          </div>
+          <motion.div
+          style={{cursor:'pointer'}}
+          whileTap={{scale:'0.9'}}
+          >
+            <MdBookmark className="text-white-50 "/>
+          </motion.div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+export default Projects;
